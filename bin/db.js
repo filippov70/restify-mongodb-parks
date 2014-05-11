fe7e7803f41e9c2dd18f917f939446a1f0d8e294
filippov70 <filippov70@gmail.com>
2014-05-11 23:04:57 +0700

    add some query files

--- bin/db.js
+++ bin/db.js
@@ -41,11 +41,15 @@ function flush_db(){
 function select_box(req, res, next){
   //clean these variables:
   var query = req.query;
-  var lat1 = Number(query.lat1).toFixed(13),
-      lon1 = Number(query.lon1).toFixed(13),
-      lat2 = Number(query.lat2).toFixed(13),
-      lon2 = Number(query.lon2).toFixed(13);
-  var limit = (typeof(query.limit) !== "undefined") ? query.limit : 400000;
+//  var lat1 = parseFloat(Number(query.lat1).toFixed(13)),
+//      lon1 = parseFloat(Number(query.lon1).toFixed(13)),
+//      lat2 = parseFloat(Number(query.lat2).toFixed(13)),
+//      lon2 = parseFloat(Number(query.lon2).toFixed(13));
+    var lat1 = parseFloat(query.lat1),
+        lon1 = parseFloat(query.lon1),
+        lat2 = parseFloat(query.lat2),
+        lon2 = parseFloat(query.lon2);
+  var limit = (typeof(query.limit) !== "undefined") ? query.limit : 4000;
   if(!(Number(query.lat1) 
     && Number(query.lon1) 
     && Number(query.lat2) 
@@ -55,13 +59,13 @@ function select_box(req, res, next){
     res.send(500, {http_status:400,error_msg: "this endpoint requires two pair of lat, long coordinates: lat1 lon1 lat2 lon2\na query 'limit' parameter can be optionally specified as well."});
     return console.error('could not connect to the database', err);
   }
-  db[collection_name].find( {'coordinates' : {'$geoWithin': { '$box' : [[lon1,lat1],[lon2,lat2]]}}}).limit(limit).toArray(function(err,rows){
+  db[collection_name].find( {'coordinates' : {'$geoWithin': { '$box' : [[lon1,lat1],[lon2,lat2]]}}}).limit(limit).toArray(function(err, rows){
     if(err) {
       res.send(500, {http_status:500,error_msg: err})
       return console.error('error running query', err);
     }
     res.send(rows);
-    console.log(rows.count);
+    console.log('rows count = ' + rows.toString());
     return rows;
   });
 };
